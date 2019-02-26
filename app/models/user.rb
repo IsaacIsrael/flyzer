d66d81a26a7a714350b6e47f132f5bd6b5e89a1e
Henrique Medeiros <hmdros@gmail.com>
2019-02-26 17:34:34 -0300

    user from google saved on our database

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,10 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     # Either create a User record or update it based on the provider (Google) and the UID
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name
+      user.remote_photo_url = auth.info.image
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
       user.token = auth.credentials.token
       user.expires = auth.credentials.expires
       user.expires_at = auth.credentials.expires_at
