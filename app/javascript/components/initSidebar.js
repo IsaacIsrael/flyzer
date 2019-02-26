670f89c3c314372bf9e037fcebccb7e1b81fc126
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-26 20:30:18 -0300

    Disabled the right side when clicking the menu

--- app/javascript/components/initSidebar.js
+++ app/javascript/components/initSidebar.js
@@ -1,6 +1,8 @@
 const initSidebar = () => {
   const sidebarCollapse = document.getElementById('sidebarCollapse');
   sidebarCollapse.addEventListener('click', (event) => {
+    const content = document.getElementById('content');
+    content.classList.toggle('disabled-div')
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('active');
   });
