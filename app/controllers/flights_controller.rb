b8343973b7c7229bb880f5fd7e5b4b7355d87f3d
isaac israel <email.iisrael@gmail.com>
2019-02-27 16:23:36 -0300

    Get location

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,5 @@ class FlightsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
   def index
     @flights = Flight.all
-    request.location
-    raise
   end
 end
