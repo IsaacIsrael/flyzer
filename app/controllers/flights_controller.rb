fb7bdd00e2a62fae96ca50b0d342202d70b2e9b4
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-28 19:06:11 -0300

    Fixed small typo in controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,6 @@
 class FlightsController < ApplicationController
   before_action :find_flight, only: %i[show]
-  skip_before_action :authenticate_user!, only: %i[index, show]
+  skip_before_action :authenticate_user!, only: %i[index show]
   def index
     @city = City.find_by(name: I18n.transliterate(request.location.city || ""))&.code
     @date = Date.today.strftime("%Y-%m-%d")
