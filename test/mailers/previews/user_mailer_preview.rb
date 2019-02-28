9673dcf7468c6690ca9d84d4c970e842a5286484
Tamara Schmidt <tamarafrrr@gmail.com>
2019-02-28 18:40:07 -0300

    Create mailer status_boarding

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/status_boarding
+  def status_boarding
+    UserMailer.status_boarding
+  end
+
+end
