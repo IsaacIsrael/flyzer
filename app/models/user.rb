3e514591a5ab3f8342d82438f20f24561f8dd762
Henrique Medeiros <hmdros@gmail.com>
2019-02-28 18:56:23 -0300

    created orders controller, adjusted orders on model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :photo, PhotoUploader
 
+  has_many :orders
+
   devise :omniauthable, :omniauth_providers => [:google_oauth2]
 
   devise :database_authenticatable, :registerable,
