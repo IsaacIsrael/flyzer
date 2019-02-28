06a1e374fb48f3bb7202a0af691be7ca49bf4846
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-28 18:24:05 -0300

    Made a private method to get the params for our flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,18 @@
 class FlightsController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  before_action :find_flight, only: %i[show]
+  skip_before_action :authenticate_user!, only: %i[index, show]
   def index
     @city = City.find_by(name: I18n.transliterate(request.location.city || ""))&.code
     @date = Date.today.strftime("%Y-%m-%d")
     @flights = Flight.sort_flights_by_date
+  end
+
+  def show
+  end
+
+  private
+
+  def find_flight
+    @flight = Flight.find(params[:id])
   end
 end
