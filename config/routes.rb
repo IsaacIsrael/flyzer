ec9271b6d20851c47f1df380682304d01d0e419b
Henrique Medeiros <hmdros@gmail.com>
2019-02-28 18:37:32 -0300

    added sku to flights

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'users/registrations' }
   root to: 'pages#home'
   resources :flights, except: :show
-  get 'flight/:id', to 'flights#show'
+  get 'flight/:id', to: 'flights#show'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_183049) do
+ActiveRecord::Schema.define(version: 2019_02_28_213702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_02_28_183049) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "sku"
     t.index ["arrival_time"], name: "index_flights_on_arrival_time"
     t.index ["company_id"], name: "index_flights_on_company_id"
     t.index ["departure_time"], name: "index_flights_on_departure_time"
