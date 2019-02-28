f4a3df7bd5364dda2ab14e5356187ac5562b2792
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-28 18:23:36 -0300

    Configured the routes to show a flight

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'users/registrations' }
   root to: 'pages#home'
-  resources :flights
+  resources :flights, except: :show
+  get 'flight/:id', to: 'flights#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :flights
 end
