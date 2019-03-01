2ec47269925c7c143d03a1c44e178ce2562dd37e
Tamara Schmidt <tamarafrrr@gmail.com>
2019-03-01 13:46:40 -0300

    Add ticket routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'tickets/index'
-  get 'tickets/show'
-  get 'tickets/new'
-  get 'tickets/create'
-  get 'tickets/destroy'
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'users/registrations' }
   root to: 'pages#home'
   resources :flights
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :flights
+  resources :tickets
 end
