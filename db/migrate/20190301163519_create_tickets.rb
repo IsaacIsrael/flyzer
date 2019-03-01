5648dc4dbfcf53364b0eb6c02ab42a174f8dd533
Tamara Schmidt <tamarafrrr@gmail.com>
2019-03-01 13:45:12 -0300

    Atualize Schema

--- db/migrate/20190301163519_create_tickets.rb
+++ db/migrate/20190301163519_create_tickets.rb
@@ -1,6 +1,8 @@
 class CreateTickets < ActiveRecord::Migration[5.2]
   def change
     create_table :tickets do |t|
+      t.string :number
+      t.string :last_name
       t.references :user, foreign_key: true
 
       t.timestamps
