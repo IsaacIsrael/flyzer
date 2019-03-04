d26cd9fc619636cc8cd07dc95e6dd960bbedb7a6
Tamara Schmidt <tamarafrrr@gmail.com>
2019-03-04 14:33:12 -0300

    Atualize interceptor email

--- lib/email_interceptor.rb
+++ lib/email_interceptor.rb
@@ -1,6 +1,6 @@
 class EmailInterceptor
   def self.delivering_email(message)
     message.subject = "#{message.to} #{message.subject}"
-    message.to = [ 'tamarafrrr@gmail.com' ]
+    message.to = ['flyzer211@gmail.com']
   end
 end
