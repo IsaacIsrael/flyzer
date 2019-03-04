d2741f247dd6408019569c2765ae6ae43f84f7b6
Tamara Schmidt <tamarafrrr@gmail.com>
2019-03-04 15:03:53 -0300

    Testing in terminal send email job

--- app/jobs/send_status_boarding_email_job.rb
+++ app/jobs/send_status_boarding_email_job.rb
@@ -1,7 +1,9 @@
 class SendStatusBoardingEmailJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    # Do something later
+  def perform()
+    puts "I'm starting the fake job"
+    sleep 3
+    puts "OK I'm done now"
   end
 end
