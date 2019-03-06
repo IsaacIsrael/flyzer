e9a1a571a9420b54b04713b0f413b2786cefd699
isaac israel <email.iisrael@gmail.com>
2019-03-06 12:23:33 -0300

    Undu

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Flyzer
   class Application < Rails::Application
-    config.middleware.insert_before ActionDispatch::Cookies, Rack::SslEnforcer
+    # config.middleware.insert_before ActionDispatch::Cookies, Rack::SslEnforcer
 
     config.generators do |generate|
           generate.assets false
