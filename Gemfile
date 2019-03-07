526fcfc1713b1794af054dc0ab2b1ae9dd9d9a1c
Henrique Medeiros <hmdros@gmail.com>
2019-03-07 13:22:57 -0300

    added yarn for datepicker

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ gem 'redis'
 gem 'cloudinary', '~> 1.9.1'
 gem 'carrierwave', '~> 1.2'
 gem 'datetime_picker_input'
-
 gem 'omniauth-google-oauth2', '~> 0.2.2'
 gem 'google-api-client', '~> 0.13.6'
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,7 @@
 import "bootstrap";
 import 'select2/dist/css/select2.css';
 import "select2-bootstrap-theme/dist/select2-bootstrap.css";
+import "../plugins/flatpickr";
 
 import { initReadUrl } from "../components/init_preview";
 import { initSidebar } from "../components/init_sidebar";
