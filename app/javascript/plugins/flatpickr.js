4fc99e7bb025b1ea4f9124083d0b9e02ee2be0b8
Henrique Medeiros <hmdros@gmail.com>
2019-03-07 16:55:46 -0300

    new ticket page ok

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,4 +1,8 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {})
+flatpickr(".datepicker", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    disableMobile: "true"
+});
