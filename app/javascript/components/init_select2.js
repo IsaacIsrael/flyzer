19f144f49cd386658852af5384d19a2960d5e5e3
isaac israel <email.iisrael@gmail.com>
2019-03-07 11:45:46 -0300

    Add the select to for the Tick form and Search Form

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -1,11 +1,37 @@
 import 'select2';
 
-const initSelect2 = () => {
-  $('.select2').select2({
-    openOnEnter: false,
-    theme: "bootstrap",
-    placeholder: "where are we traveling"
+
+const initSelect2Ticket = () =>{
+  const element = document.getElementById('ticket');
+  if (element) {
+
+    $('.select2.places').select2({
+      theme: "bootstrap",
+      placeholder: "Aeroport"
+    });
+
+  }
+}
+
+const initSelect2Search = () => {
+  const element = document.getElementById('search');
+  if(element){
+    const select =  $('.select2');
+    select.select2({
+      theme: "bootstrap",
+      placeholder: "where are we traveling"
+    });
+   select.on("change", function (e) {
+      $(this).closest('form').submit();
   });
+  }
+};
+
+
+
+const initSelect2 = () => {
+  initSelect2Search();
+  initSelect2Ticket();
 };
 
 export { initSelect2 };
