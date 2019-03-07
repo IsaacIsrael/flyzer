9fd02b440392b4507ba279c2864868aa57e4d7e9
isaac israel <email.iisrael@gmail.com>
2019-03-07 12:00:10 -0300

    Select2 for companies

--- app/javascript/components/init_select2.js
+++ app/javascript/components/init_select2.js
@@ -4,29 +4,29 @@ import 'select2';
 const initSelect2Ticket = () =>{
   const element = document.getElementById('ticket');
   if (element) {
-
     $('.select2.places').select2({
       theme: "bootstrap",
       placeholder: "Aeroport"
     });
-
+     $('.select2.companies').select2({
+       theme: "bootstrap"
+     });
   }
 }
 
 const initSelect2Search = () => {
   const element = document.getElementById('search');
   if(element){
-    const select =  $('.select2');
-    select.select2({
+    const search =  $('.select2');
+    search.select2({
       theme: "bootstrap",
       placeholder: "where are we traveling"
     });
-   select.on("change", function (e) {
+    search.on("change", function (e) {
       $(this).closest('form').submit();
-  });
+    });
   }
 };
-
 
 
 const initSelect2 = () => {
