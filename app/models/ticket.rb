50eaa11423fe4dd488feaf57a0795a2431b630b2
isaac israel <email.iisrael@gmail.com>
2019-03-08 13:37:51 -0300

    Impliment sweetalert

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -6,4 +6,15 @@ class Ticket < ApplicationRecord
   belongs_to :destination, class_name: 'Place'
 
   validates :number, :departure_time, :arrival_time, presence: true
+
+  def json
+    {
+      id: id,
+      number: number,
+      origin: origin.code,
+      destination: destination.code,
+      arrival_time: arrival_time,
+      departure_time: departure_time
+    }.to_json
+  end
 end
