d5e77079dfc4eeb44ab50669a603bc7c323adc02
Fabricio Bravo <fgbravo188@gmail.com>
2019-03-08 17:26:06 -0300

    Finished the javascript greeting

--- app/javascript/components/init_get_hour.js
+++ app/javascript/components/init_get_hour.js
@@ -10,9 +10,9 @@ const initGetHour = () => {
   const greeting = document.getElementById("greeting");
   const hour = (date.getHours());
   console.log(hour);
-  if (0 > hour > 12) {
+  if (0 >= hour > 12) {
     greeting.innerHTML = "Good morning,";
-  } else if (18 > hour > 23) {
+  } else if (18 >= hour > 24) {
     greeting.innerHTML = "Good evening,";
   } else {
     greeting.innerHTML = "Good afternoon,";
