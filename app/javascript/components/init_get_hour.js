31381c6b77f3e87c5f55bd95a114388513a22514
Fabricio Bravo <fgbravo188@gmail.com>
2019-03-11 15:20:44 -0300

    Removed the console logs'

--- app/javascript/components/init_get_hour.js
+++ app/javascript/components/init_get_hour.js
@@ -4,16 +4,12 @@ const initGetHour = () => {
     const date = new Date();
     const greeting = document.getElementById("greeting");
     const hour = (date.getHours());
-    console.log(hour);
     if (hour >= 0 && hour < 12) {
       greeting.innerHTML = "Good morning,";
-      console.log("Manha")
     } else if (hour >= 18 && hour <= 24) {
       greeting.innerHTML = "Good evening,";
-      console.log("noite")
     } else {
       greeting.innerHTML = "Good afternoon,";
-      console.log("tarde")
     }
   }
 }
